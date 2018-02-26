eaf9b876760f094934c4b03c9156377ddf2872bb
Alex Mallick <alm4@graceland.edu>
2018-02-26 00:35:41 -0600

    lab 5

--- lab5.py
+++ lab5.py
@@ -12,24 +12,28 @@ def word_finder(x):
             print(i)
             break
 
-list1=wordlist.words
-n=len(list1)-1
-y=n
-
-def word_finder1(x):
-    global n
-    global y
-    if list1[n].lower()==x.lower():
-        return n
-    else:
-        if list1[round(n/2)].lower()<x.lower():
-            n=round((n+y)/2)
-            y=n
-        elif list1[round(n/2)].lower()>x.lower():
-            n=round(((n/2)+y)/2)
-            y=round(y*)
-        print(n)
-        return word_finder1(x)
-    
-    
+##list1=wordlist.words
+##n=len(list1)-1
+##y=n
+##z=0
+##
+##def word_finder1(x):
+##    global n
+##    global y
+##    global z
+##    if list1[n].lower()==x.lower():
+##        return n
+##    else:
+##        if list1[round(n/2)].lower()<x.lower():
+##            y=n/2
+##            
+##            n=round(n/4)
+##        elif list1[round(n/2)].lower()>x.lower():
+##            n=round(((n/2)+y)/2)
+##            y=
+##            
+##        print(n)
+##        return word_finder1(x)
+##    
+##    
  
